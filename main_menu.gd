e9cd83bace18cbb643fdfc154a2222432117a372
Orangeangelo <evasan46@kingshigh.school.nz>
2025-05-29 10:49:36 +1200

    Menu V1

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed():
+	print("Start pressed")
+
+
+func _on_settings_pressed():
+	print("Settings pressed")
+
+
+func _on_exit_pressed():
+	print("Exit pressed")
