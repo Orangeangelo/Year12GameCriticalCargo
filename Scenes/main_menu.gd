3548af3293513d0ffc333b662ca00f5ee26f5412
Orangeangelo <evasan46@kingshigh.school.nz>
2025-06-03 13:26:24 +1200

    Map/Level V1
    Added background
    Added tiles
    Played with the tiles
    Fixed some code

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
 
 
 func _on_settings_pressed():
