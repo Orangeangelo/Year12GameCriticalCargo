ea2d73ad9df8c0c5a8ed5675cb977ec3b799b6c2
Orangeangelo <evasan46@kingshigh.school.nz>
2025-06-27 12:01:38 +1200

    Character V2
    Added inputs. changed speed and jump velocity. Finished the basics of the character.

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -600.0
+@onready var sprite_2d = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
+	#Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "idle"
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		sprite_2d.animation = "jump"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
