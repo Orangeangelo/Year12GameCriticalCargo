ff6987ce0b2e20100733ff15f647e61baa270be0
Orangeangelo <evasan46@kingshigh.school.nz>
2025-07-31 14:42:52 +1200

    Character V3
    added double jump and double jump animation

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -1,30 +1,35 @@
 extends CharacterBody2D
 
-
 const SPEED = 425.0
 const JUMP_VELOCITY = -750.0
+var jump_count = 0
 @onready var sprite_2d = $Sprite2D
 
-
 func _physics_process(delta: float) -> void:
-	#Animations
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "running"
+	# Animations
+	if not is_on_floor():
+		if jump_count == 1:
+			sprite_2d.animation = "jump"
+		elif jump_count == 2:
+			sprite_2d.animation = "double_jumping"
 	else:
-		sprite_2d.animation = "idle"
+		if velocity.x > 1 || velocity.x < -1:
+			sprite_2d.animation = "running"
+		else:
+			sprite_2d.animation = "idle"
 	
-	
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		sprite_2d.animation = "jump"
+	else:
+		jump_count = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/deceleration
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
