6b54cd0f434fabefab8943205f161ddb431d012c
Orangeangelo <evasan46@kingshigh.school.nz>
2025-08-14 22:35:59 +1200

    Pause Menu and Settings
    I added a settings which only allows you to change the volume of the music

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $"Main buttons"
+@onready var settings: Panel = $settings
+
+
+func _ready():
+	main_buttons.visible = true
+	settings.visible = false
+
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
@@ -7,7 +15,13 @@ func _on_start_pressed():
 
 func _on_settings_pressed():
 	print("Settings pressed")
+	main_buttons.visible = false
+	settings.visible = true
 
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_back_settings_pressed() -> void:
+	_ready()
