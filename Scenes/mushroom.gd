eeb4854c3af1a60a07e8c3805eaed364c71b0cef
Orangeangelo <evasan46@kingshigh.school.nz>
2025-09-09 12:10:19 +1200

    Enemy V2
    Just added the basic patrolling. Moving left to right.

--- Scenes/mushroom.gd
+++ Scenes/mushroom.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
-const SPEED = 75.0
+const SPEED = 200.0
 
 var direction = 1
 var health = 1
 
 func add_gravity(delta):
 	if not is_on_floor():
-		velocity.y += get_gravity() * delta
-
-
+		velocity += get_gravity() * delta
 
 func move_enemy():
 	velocity.x = SPEED * direction
+
+func reverse_direction():
+	if is_on_wall():
+		direction = -direction
+
+func _physics_process(delta: float) -> void:
+	add_gravity(delta)
+	move_enemy()
+	move_and_slide()
+	reverse_direction()
