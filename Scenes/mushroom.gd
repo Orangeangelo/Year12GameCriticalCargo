d4a8757ff48a74a60d057e40ab9ab6bd09d72b6a
Orangeangelo <evasan46@kingshigh.school.nz>
2025-09-16 18:40:06 +1200

    New assets

--- Scenes/mushroom.gd
+++ Scenes/mushroom.gd
@@ -9,6 +9,13 @@ func add_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+func platform_edge():
+	if not $RayCast2D.is_colliding():
+		direction = -direction
+		$RayCast2D.position.x = -1
+		
+		
+
 func move_enemy():
 	velocity.x = SPEED * direction
 
@@ -21,3 +28,4 @@ func _physics_process(delta: float) -> void:
 	move_enemy()
 	move_and_slide()
 	reverse_direction()
+	platform_edge()
