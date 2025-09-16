f2a26dfbc7daa2d96e8b687baab788528fdbfeb3
Orangeangelo <evasan46@kingshigh.school.nz>
2025-09-17 11:38:53 +1200

    Traps/Checkpoint
    Made spike trap faster and fixed Checkpoint animation

--- Scenes/checkpoint.gd
+++ Scenes/checkpoint.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 func _ready() -> void:
 	$AnimationPlayer.play("Checkpoint")
+	# Connect the animation_finished signal
+	$AnimationPlayer.connect("animation_finished", _on_animation_finished)
 
 func _process(delta: float) -> void:
 	if GameStats.get_spawn() != self:
@@ -11,3 +13,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		GameStats.set_spawn(self)
 		$AnimationPlayer.play("saved")
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "saved":
+		$AnimationPlayer.play("Checkpoint")
